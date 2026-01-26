534f245d57924a72fd18fa93f264bc24f8b9dfc9
Josh Tregenza <josh@joshtregenza.com>
2026-01-26 17:19:28 +1000

    updating the nav

--- components/nav.tsx
+++ components/nav.tsx
@@ -1,8 +1,9 @@
 import Link from "next/link";
+import styles from './style/nav.module.css'
 
 export default function Nav() {
   return (
-    <header>
+    <header className={styles.header}>
         <Link href="/">Home</Link>
         <Link href="/projects">Portfolio</Link>
         <Link href="/voice">Voice Acting</Link>
